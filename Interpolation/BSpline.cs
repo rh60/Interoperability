e575304ea421366fef5aab9f6928a30c632247e5
rh60 <32161865+rh60@users.noreply.github.com>
2019-01-06 10:07:19 +0100

    Add files via upload

---
+++ Interpolation/BSpline.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMP
+{
+    public class BSpline
+    {
+        double[] U; // knots (Uzly)
+        int p; // order (Řád)
+        public BSpline(params double[] knots)
+        {
+            U = knots;
+            p = 0;
+            for (int i = 1; i < U.Length; i++)
+            {
+                if (U[i] == U[i - 1])
+                    p++;
+                else
+                    break;
+            }
+        }
+
+        private int End => U.Length - p - 1;
+
+        public int FindSpan(double u)
+        {
+            if (u == U[End])
+                return End - 1;
+            int low = p, high = End;
+            int mid = (low + high) / 2;
+            while (u < U[mid] || u >= U[mid+1])
+            {
+                if (u < U[mid]) high = mid;
+                else low = mid;
+                mid = (low + high) / 2;
+            }
+            return mid;
+        }
+
+        public double[] BasisFuns(int i, double u)
+        {
+            double left(int j)
+            {
+                return u - U[i + 1 - j];
+            }
+
+            double right(int j)
+            {
+                return U[i + j] - u;
+            }
+
+            double[] N = new double[p+1];
+            N[0] = 1.0;
+            for (int j = 1; j <= p; j++)
+            {               
+                double saved = 0.0;
+                for (int k = 0; k < j; k++)
+                {
+                    var temp = N[k] / (right(k + 1) + left(j - k));
+                    N[k] = saved + right(k + 1) * temp;
+                    saved = left(j - k) * temp;
+                }
+                N[j] = saved;
+            }
+            return N;
+        }
+
+    }
+}
---
+++ Interpolation/PeriodicSpline.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using vec = MathNet.Numerics.LinearAlgebra.Vector<double>;
+using mat = MathNet.Numerics.LinearAlgebra.Matrix<double>;
+using MathNet.Numerics.LinearAlgebra;
+using MMP.Algebra.DoubleDouble;
+
+namespace MMP.Interpolation
+{
+    public class PeriodicSpline
+    {        
+        vec h;
+        Func<double, double>[,] S;
+
+        public PeriodicSpline(vec t, params vec[] x)
+        {            
+            var n = t.Count - 1;
+            var nx = x.Length;
+            var X = mat.Build.DenseOfColumnVectors(x);
+
+            var h0 = t.Diff();
+            var h = vec.Build.Dense(t.Count);
+            h0.CopySubVectorTo(h, 0, 0, n); h[n] = h0[0];
+            var h1 = h.SubVector(1, n);
+            this.h = h.SubVector(0, n); ;
+            var A = mat.Build.SparseOfDiagonalVector(h0 + h1);
+            A.SetDiag(h1, 1); A[0, n - 1] = h0[0];
+            A += A.Transpose();
+
+            X = X.Stack(X.SubMatrix(1, 1, 0, X.ColumnCount));
+            var dX = X.Diff();
+            dX = mat.Build.DenseOfColumnVectors(dX.EnumerateColumns().Select(par => par.PointwiseDivide(h)));
+            var B = 3 * dX.Diff();
+            var M = A.Solve(B);
+            M = M.SubMatrix(M.RowCount - 1, 1, 0, M.ColumnCount).Stack(M);
+
+            //Console.WriteLine(M);
+
+            S = new Func<double, double>[n,nx];
+
+
+            for (int j = 0; j < nx; j++)
+            {
+                var a2 = M.Column(j);
+
+                var a0 = X.Column(j);
+                
+                for (int i = 0; i < n; i++)
+                {
+                    var a3 = (a2[i + 1] - a2[i]) / h[i] / 3;
+                    var a1 = (a0[i + 1] - a0[i]) / h[i]-(2*a2[i]+a2[i+1])*h[i]/3;
+                    S[i,j] = Horner.CreateEvaluator(a3,a2[i],a1, a0[i]);
+                }
+            }
+        }
+
+        public double[][] Interpolate(int n)
+        {
+            int N = n*h.Count;
+            int ndim = S.GetLength(1);
+            var R = new double[ndim][];
+            for (int dim = 0; dim < ndim; dim++)
+            {
+                R[dim] = new double[N]; 
+            }
+            int pos = 0;
+            for (int i = 0; i < h.Count; i++)
+            {
+                double s = 0;
+                double dh = h[i] / n;
+                for (int j = 0; j < n; j++)
+                {                    
+                    for (int dim = 0; dim < ndim; dim++)
+                    {
+                        R[dim][pos] = S[i, dim](s);
+                    }
+                    pos++; s += dh;
+                }
+            }
+            return R;
+        }           
+
+    }
+}
