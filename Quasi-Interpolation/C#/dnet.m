3537d936c444e56f86ba0270c3909725746fbca7
rh60 <32161865+rh60@users.noreply.github.com>
2019-01-10 12:32:26 +0100

    Add files via upload

--- Quasi-Interpolation/C#/dnet.m
+++ Quasi-Interpolation/C#/dnet.m
@@ -1,7 +1,21 @@
-ff=fullfile(pwd,'BSpline.dll');
+clear;clc;close;
+ff=fullfile(pwd,'CsLib.dll');
 a=NET.addAssembly(ff);
-bs=MMP.BSpline([0, 0, 0, 1, 1, 1]);
+bs=MMP.BSpline([0, 0, 0, 1, 2, 3, 4, 4, 5, 5, 5]);
 pp=bs.Bases();
+hold on
+for i=1:pp.Count
+   n=pp.Item(i-1).Count;
+   x=zeros(1,n);
+   y=x;
+   for j=1:n
+       p=pp.Item(i-1).Item(j-1);
+       x(j)=p.x;
+       y(j)=p.y;       
+   end
+   plot(x,y);
+end
 
-c=pp.Item(0);
-c.ToArray()
+
+
+
